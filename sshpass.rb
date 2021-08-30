f1972ba04b146cf122511b6fe7aafa8c8bcb567b
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2021-08-30 14:40:29 +0200

    Fixed a stupid typo error in installer

--- sshpass.rb
+++ sshpass.rb
@@ -5,7 +5,7 @@ class Sshpass < Formula
   url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
   sha256 'cbc0e1300c7f1cacf9c850e5b42773cb53211d9d'
-  licence 'MIT'
+  license 'MIT'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
