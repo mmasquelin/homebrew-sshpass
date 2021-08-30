bdfbb90f74315e255ab9e5292a43bb8c4b267c29
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2021-08-30 14:41:52 +0200

    Fixed sha256 sum

--- sshpass.rb
+++ sshpass.rb
@@ -4,7 +4,7 @@ class Sshpass < Formula
   desc 'The sshpass utility helps administrators more easily manage SSH connections in scripts.'
   url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
-  sha256 'cbc0e1300c7f1cacf9c850e5b42773cb53211d9d'
+  sha256 '71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7'
   license 'MIT'
 
   def install
