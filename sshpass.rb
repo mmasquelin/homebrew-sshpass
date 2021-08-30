c7948295bc431aa30f06bfd11ec4fe4d15608df5
Mickael MASQUELIN <mickael.masquelin@univ-lille.fr>
2021-08-30 14:24:03 +0200

    Added description in dot rb sshpass file

--- sshpass.rb
+++ sshpass.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Sshpass < Formula
+  desc 'The sshpass utility helps administrators more easily manage SSH connections in scripts.'
   url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
   sha1 'cbc0e1300c7f1cacf9c850e5b42773cb53211d9d'
+  licence 'MIT'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
